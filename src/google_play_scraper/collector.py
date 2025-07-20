33efe6d5aa78e727679a98deab9ce0c78e8dc7bc
Artur Maslov <aartasas@gmail.com>
2025-07-20 19:50:16 +0300

    add query to csv export start

--- src/google_play_scraper/collector.py
+++ src/google_play_scraper/collector.py
@@ -28,9 +28,9 @@ class GooglePlayDataCollector:
         self._output_file = output_file if output_file else DEFAULT_OUTPUT_FILE
         self._logger = logger if logger else logging.getLogger(__name__)
 
-    def _save_to_csv(self, items: List[PlayItem], category: GooglePlayCategory) -> None:
+    def _save_to_csv(self, query: str, items: List[PlayItem], category: GooglePlayCategory) -> None:
         """Saves given list of play items to a CSV file."""
-        output_file_name = f"{category.value}_{self._output_file}"
+        output_file_name = f"{query}_{category.value}_{self._output_file}"
         self._logger.info(f"Writing {len(items)} items to {output_file_name}..")
         play_items = [item.payload.model_dump() for item in items]
         df = pd.DataFrame(play_items)
@@ -59,4 +59,4 @@ class GooglePlayDataCollector:
             self._logger.info("No items found for query.")
             return
 
-        self._save_to_csv(items, category)
+        self._save_to_csv(query, items, category)
